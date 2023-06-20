30f4ddc800e040a17f2115fa8644b8227d34184a
jjjgm <businessjgm@icloud.com>
2023-06-20 12:38:13 -0700

    installing react-router-dom & adjustments to index.html

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # working-portfolio
 a working portfolio showcasing projects &amp; technical skills
+
+## Installation
+- React
+- react-router-dom
+- Google Fonts
---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          THIS IS A SAMPLE HEADER TO CHECK FOR DEPLOYMENT
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
