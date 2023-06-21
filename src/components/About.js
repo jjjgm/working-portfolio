f6276824cb581f0fd65f0d21c4698ed47fe15278
jjjgm <businessjgm@icloud.com>
2023-06-21 15:01:11 -0700

    feature/ grid-about

--- src/components/About.js
+++ src/components/About.js
@@ -5,16 +5,22 @@ import './styles/About.css';
 function About() {
     return (
         <div>
-            <div className="about-section">
-                <h1>A BIT ABOUT ME</h1>
+            <div className="about-grid">
 
 
-                <h2>Hi, I'm Jennifer!</h2>
-                <p>Growing up in the San Gabriel Valley region of Southern California, helped me develop a deep appreciation for the local mountain ranges, which fueled my love for the outdoors.</p>
+                <h5>A BIT ABOUT ME</h5>
 
-                <p>In addition to coding, I find joy in exploring local museums, cycling with friends, listening to podcasts, solving puzzles, and occasionally indulging in evening gaming sessions. I also enjoy challenging myself on my gravel bike and maximizing my passion for organization around the home.</p>
 
-                <p>I am thrilled to connect with like-minded peers and contribute to impactful projects. Let's collaborate and create something truly amazing together!</p>
+                {/* image */}
+                <figure>
+                    <p className="about-text">Growing up in the San Gabriel Valley region of Southern California, helped me develop a deep appreciation for the local mountain ranges, which fueled my love for the outdoors.
+                    <br />
+                    <br />
+                   In addition to coding, I find joy in exploring local museums, cycling with friends, listening to podcasts, solving puzzles, and occasionally indulging in evening gaming sessions. I also enjoy challenging myself on my gravel bike and maximizing my passion for organization around the home.
+                   <br />
+                   <br />
+                   I am thrilled to connect with like-minded peers and contribute to impactful projects. Let's collaborate and create something truly amazing together!</p>
+                </figure>
             </div>
         </div>
     )
--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,48 +1,101 @@
 import React from 'react';
 
 import './styles/Portfolio.css';
+import { DiGithubFull } from "react-icons/di";
 
 function Portfolio() {
     return (
-<div className="portfolio-grid">
+        <div className="portfolio-grid">
 
-<figure>
-{/* image */}
-	<caption>Project 1</caption>
-    <p className="figure-text">Lorem, ipsum dolor sit amet consectetur adipisicing elit labore rem illum totam dolorem quam saepe harum reprehenderit consequatur fugit praesentium dolore delectus modi, veritatis debitis officiis enim blanditiis facere!</p>
-</figure>
+            <figure>
+                {/* image */}
+                <caption>
+                    <a href="https://github.com/jjjgm/blogging-techfluencer">
+                        <h3>Techfluencer Blog</h3>
+                    </a>
+                </caption>
+                <p className="figure-text">
+                    A blog for tech related posts. Ability to comment (once logged in) on other posts, delete your posts, and previous comments.
+                </p>
+                <a href="https://github.com/jjjgm/blogging-techfluencer">
+                        <h4 className="figure-icon"><DiGithubFull/></h4>
+                    </a>
+            </figure>
 
-<figure>
-	{/* image */}
-	<caption>Project 2</caption>
-    <p className="figure-text">Lorem, ipsum dolor sit amet consectetur adipisicing elit labore rem illum totam dolorem quam saepe harum reprehenderit consequatur fugit praesentium dolore delectus modi, veritatis debitis officiis enim blanditiis facere!</p>
-</figure>
+            <figure>
+                {/* image */}
+                <caption>
+                    <a href="https://github.com/jjjgm/jjjgm-reactfolio">
+                        <h3>Student Portfolio</h3>
+                    </a>
+                </caption>
+                <p className="figure-text">
+                Stylist.Ai generates a three piece ensemble based off of any occasion, event, or setting that a 'user' requests. The 'user' has the option to shop for and save their favorite pieces.
+                </p>
+                <a href="https://github.com/jjjgm/blogging-techfluencer">
+                        <h4 className="figure-icon"><DiGithubFull/></h4>
+                    </a>
+            </figure>
 
-<figure>
-	{/* image */}
-	<caption>Project 3</caption>
-    <p className="figure-text">Lorem, ipsum dolor sit amet consectetur adipisicing elit labore rem illum totam dolorem quam saepe harum reprehenderit consequatur fugit praesentium dolore delectus modi, veritatis debitis officiis enim blanditiis facere!</p>
-</figure>
+            <figure>
+                {/* image */}
+                <caption>
+                    <a href="https://github.com/jjjgm/the-back-of-the-shop">
+                        <h3>'Server Side' E-Commerce</h3>
+                    </a>
+                </caption>
+                <p className="figure-text">
+                    ORM database for an E-commerce platform that showcases table relationships. 
+                </p>
+                <a href="https://github.com/jjjgm/blogging-techfluencer">
+                        <h4 className="figure-icon"><DiGithubFull/></h4>
+                    </a>
+            </figure>
 
-<figure>
-	{/* image */}
-	<caption>Project 4</caption>
-    <p className="figure-text">Lorem, ipsum dolor sit amet consectetur adipisicing elit labore rem illum totam dolorem quam saepe harum reprehenderit consequatur fugit praesentium dolore delectus modi, veritatis debitis officiis enim blanditiis facere!</p>
-</figure>
+            <figure>
+                {/* image */}
+                <caption>
+                    <a href="https://autohome.github.io/jenhua-joerdo/">
+                        <h3>Stylist.Ai</h3>
+                    </a>
+                </caption>
+                <p className="figure-text">Lorem, ipsum dolor sit amet consectetur adipisicing elit labore rem illum totam dolorem quam saepe harum reprehenderit consequatur fugit praesentium dolore delectus modi, veritatis debitis officiis enim blanditiis facere!</p>
+                <a href="https://github.com/jjjgm/blogging-techfluencer">
+                        <h4 className="figure-icon"><DiGithubFull/></h4>
+                    </a>
+            </figure>
 
-<figure>
-	{/* image */}
-	<caption>Project 5</caption>
-    <p className="figure-text">Lorem, ipsum dolor sit amet consectetur adipisicing elit labore rem illum totam dolorem quam saepe harum reprehenderit consequatur fugit praesentium dolore delectus modi, veritatis debitis officiis enim blanditiis facere!</p>
-</figure>
+            <figure>
+                {/* image */}
+                <caption>
+                    <a href="https://github.com/Kriskris9/BandMatch">
+                        <h3>BandMatch</h3>
+                    </a>
+                </caption>
+                <p className="figure-text">
+                A posting board and social network for musically inclined individuals to seek new bandmates, jam sessions, or connect over similar musical interests. Your new 'Bandmatch' awaits.
+                </p>
+                <a href="https://github.com/jjjgm/blogging-techfluencer">
+                        <h4 className="figure-icon"><DiGithubFull/></h4>
+                    </a>
+            </figure>
 
-<figure>
-	{/* image */}
-	<caption>Project 6</caption>
-    <p className="figure-text">Lorem, ipsum dolor sit amet consectetur adipisicing elit labore rem illum totam dolorem quam saepe harum reprehenderit consequatur fugit praesentium dolore delectus modi, veritatis debitis officiis enim blanditiis facere!</p>
-</figure>
+            <figure>
+                {/* image */}
+                <caption>
+                    <a href="https://github.com/jjjgm/gg-get-icecream" >
+                        <h3>Thank You for Being a Friend</h3>
+                    </a>
+                </caption>
+                <p className="figure-text">
+                A social-network profile/board for a 'user' and their pet(s). Match-up your furry friends and plan your to next adventure together!
+                </p>
+                <a href="https://github.com/jjjgm/blogging-techfluencer">
+                        <h4 className="figure-icon"><DiGithubFull/></h4>
+                    </a>
+            </figure>
 
-</div>
+        </div>
 
     )
 }
